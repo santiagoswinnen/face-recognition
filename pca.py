7d1e74ef9408845c5491a96cb963cc03116b5dc6
FacuVarela <favarela@itba.edu.ar>
2020-10-04 19:44:51 +0200

    Adding KPCA

--- pca.py
+++ pca.py
@@ -11,7 +11,8 @@ from sklearn import svm
 IMAGES_PER_PERSON = 3
 
 training_dataset = 'training_set/'
-trained_file = 'trained.txt'
+trained_file_pca = 'trained_pca.txt'
+trained_file_kpca = 'trained_kpca.txt'
 folders = [f for f in listdir(training_dataset)]
 
 print(listdir(training_dataset))
@@ -26,9 +27,9 @@ image_area = width * height
 
 
 def pca_train():
-    trained = Path(trained_file)
+    trained = Path(trained_file_pca)
     if trained.is_file():
-        print('Successfully loaded {}'.format(trained_file))
+        print('Successfully loaded {}'.format(trained_file_pca))
         return np.loadtxt(trained)
 
     all_faces, person, image_index = parse_faces()
@@ -49,26 +50,37 @@ def pca_train():
     for i in range(m):
         C_eigenvectors[:, i] /= np.linalg.norm(C_eigenvectors[:, i])
 
-    np.savetxt(trained_file, C_eigenvectors, fmt='%s')
+    np.savetxt(trained_file_pca, C_eigenvectors, fmt='%s')
     return C_eigenvectors
 
 
 def kpca_train():
-    all_faces = parse_faces()
+    trained = Path(trained_file_kpca)
+    if trained.is_file():
+        print('Successfully loaded {}'.format(trained_file_kpca))
+        return np.loadtxt(trained)
+
+    all_faces, person, image_index = parse_faces()
+
     degree = 2
 
     K = (np.dot(all_faces, np.transpose(all_faces)) + 1) ** degree
 
+    #Center the original images
     ones = np.ones([trainings, trainings]) / trainings
+
     K_dot_ones = np.dot(K, ones)
-    K = K - np.dot(ones, K) - K_dot_ones + np.dot(ones, K_dot_ones)
+    ones_dot_K = np.dot(ones, K)
+    ones_dot_K_dot_ones = np.dot(ones, K_dot_ones)
+
+    K = K - ones_dot_K - K_dot_ones + ones_dot_K_dot_ones
 
     C_eigenvectors = find_eigenvectors(np.array(K), np.array(K))
 
     for i in range(len(K)):
         C_eigenvectors[:, i] /= np.linalg.norm(C_eigenvectors[:, i])
 
-    np.savetxt(trained_file, C_eigenvectors, fmt='%s')
+    np.savetxt(trained_file_kpca, C_eigenvectors, fmt='%s')
 
     return C_eigenvectors
 
@@ -135,7 +147,49 @@ def pca_face_input(eigenfaces, input_image):
 
 
 def kpca_face_input(eigenfaces, face_img):
-    return
+    all_faces, person, last_index = parse_faces()
+
+    #Test set
+    test_number = 1
+    test_set = np.zeros([test_number,image_area])
+    image_index = 0
+
+    # Adapting new image to match in format with the rest (normalized and with vector shape)
+    a = face_image / 255.0
+
+    # Reshape to vector for insertion in 'images'
+    test_set[image_index, :] = np.reshape(a, [1, image_area])
+    
+    #KERNEL: polinomial degree
+    degree = 2
+    K = (np.dot(all_faces, np.transpose(all_faces)) + 1)**degree
+            
+    #esta transformación es equivalente a centrar las imágenes originales...
+    ones = np.ones([trainings, trainings]) / trainings
+    K_dot_ones = np.dot(K, ones)
+    K = K - np.dot(ones, K) - K_dot_ones + np.dot(ones, K_dot_ones)
+
+    #pre-proyección
+    set_pre_proyection = np.dot(np.transpose(K),eigenfaces)
+    unoML = np.ones([test_number,trainings])/trainings
+    Ktest = (np.dot(test_set,np.transpose(all_faces))/trainings+1)**degree
+    ones_dot_K = np.dot(unoML, K)
+    Ktest_dot_ones = np.dot(Ktest,ones)
+    ones_dot_K_dot_ones = np.dot(unoML,K_dot_ones)
+    Ktest = Ktest - ones_dot_K - Ktest_dot_ones + ones_dot_K_dot_ones
+    test_set_pre_proyect = np.dot(Ktest,eigenfaces)
+    
+    nmax = eigenfaces.shape[1]
+    nmax = 100
+    set_proyection = set_pre_proyection[:,0:nmax]
+    test_set_proyection = test_set_pre_proyection[:,0:nmax]
+            
+    #SVM
+    clf = svm.LinearSVC()
+    clf.fit(set_proyection,person.ravel())
+    labels = clf.predict(test_set_proyection)
+
+    return labels[0]
 
 
 def show_average_face(av_face):
